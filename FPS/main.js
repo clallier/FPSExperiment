1cbb12429da9f964645bb69df73ee099d8d64203
clallier <c.lallier@gmail.com>
2015-05-09 23:54:28 +0200

    JSPM.io and ES6 (babel) integration

--- FPS/main.js
+++
@@ -1,9 +0,0 @@
-
-// http://www.isaacsukin.com/news/2012/06/how-build-first-person-shooter-browser-threejs-and-webglhtml5-canvas
-
-/**
-  * On load
-  */
-var retroBomber = Object.create(RetroBomber);
-
-window.addEventListener("load", retroBomber.init() );
